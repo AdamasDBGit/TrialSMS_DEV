b0daa628d3244b64188bdb0fc5272930e8c2b60b
AdamasDBGit <abhik.porel@adamastech.in>
2024-05-09 11:30:22 +0530

    add linked Server

--- SMS_Trial_Dev/Linked Server/PROD.sql
+++ SMS_Trial_Dev/Linked Server/PROD.sql
@@ -1,3 +1,50 @@
-﻿EXECUTE sp_addlinkedserver @server = N'PROD'
+﻿USE [master]
 GO
-EXECUTE sp_addlinkedsrvlogin @rmtsrvname = N'PROD'
+
+/****** Object:  LinkedServer [PROD]    Script Date: 09-05-2024 11:29:30 ******/
+EXEC master.dbo.sp_addlinkedserver @server = N'PROD', @srvproduct=N'Oracle', @provider=N'OraOLEDB.Oracle', @datasrc=N'//192.168.1.212:1531/PROD'
+ /* For security reasons the linked server remote logins password is changed with ######## */
+EXEC master.dbo.sp_addlinkedsrvlogin @rmtsrvname=N'PROD',@useself=N'False',@locallogin=NULL,@rmtuser=N'rice',@rmtpassword='########'
+GO
+
+EXEC master.dbo.sp_serveroption @server=N'PROD', @optname=N'collation compatible', @optvalue=N'false'
+GO
+
+EXEC master.dbo.sp_serveroption @server=N'PROD', @optname=N'data access', @optvalue=N'true'
+GO
+
+EXEC master.dbo.sp_serveroption @server=N'PROD', @optname=N'dist', @optvalue=N'false'
+GO
+
+EXEC master.dbo.sp_serveroption @server=N'PROD', @optname=N'pub', @optvalue=N'false'
+GO
+
+EXEC master.dbo.sp_serveroption @server=N'PROD', @optname=N'rpc', @optvalue=N'false'
+GO
+
+EXEC master.dbo.sp_serveroption @server=N'PROD', @optname=N'rpc out', @optvalue=N'false'
+GO
+
+EXEC master.dbo.sp_serveroption @server=N'PROD', @optname=N'sub', @optvalue=N'false'
+GO
+
+EXEC master.dbo.sp_serveroption @server=N'PROD', @optname=N'connect timeout', @optvalue=N'0'
+GO
+
+EXEC master.dbo.sp_serveroption @server=N'PROD', @optname=N'collation name', @optvalue=null
+GO
+
+EXEC master.dbo.sp_serveroption @server=N'PROD', @optname=N'lazy schema validation', @optvalue=N'false'
+GO
+
+EXEC master.dbo.sp_serveroption @server=N'PROD', @optname=N'query timeout', @optvalue=N'0'
+GO
+
+EXEC master.dbo.sp_serveroption @server=N'PROD', @optname=N'use remote collation', @optvalue=N'true'
+GO
+
+EXEC master.dbo.sp_serveroption @server=N'PROD', @optname=N'remote proc transaction promotion', @optvalue=N'true'
+GO
+
+
+
